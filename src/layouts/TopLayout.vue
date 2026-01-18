c0dc33144fd1db416fa368c8de4e98257d285472
Ray.Hao <1490493387@qq.com>
2026-01-18 13:17:08 +0800

    fix: 修复 TopLayout 顶部横向菜单提前省略

--- src/layouts/TopLayout.vue
+++ src/layouts/TopLayout.vue
@@ -3,8 +3,12 @@
     <!-- 顶部菜单栏 -->
     <div class="layout__header">
       <div class="layout__header-left">
-        <LayoutLogo v-if="showLogo" :collapse="isLogoCollapsed" />
-        <LayoutSidebar :data="routes" menu-mode="horizontal" base-path="" />
+        <div v-if="showLogo" class="layout__header-logo">
+          <LayoutLogo :collapse="isLogoCollapsed" />
+        </div>
+        <div class="layout__header-menu">
+          <LayoutSidebar :data="topMenuItems" menu-mode="horizontal" base-path="" />
+        </div>
       </div>
       <div class="layout__header-right">
         <LayoutToolbar />
@@ -22,6 +26,7 @@
 <script setup>
 import { useWindowSize } from "@vueuse/core";
 import { useLayout } from "./useLayout";
+import { usePermissionStore } from "@/store";
 import BaseLayout from "./BaseLayout.vue";
 import LayoutLogo from "./components/LayoutLogo.vue";
 import LayoutSidebar from "./components/LayoutSidebar.vue";
@@ -29,8 +34,14 @@ import LayoutToolbar from "./components/LayoutToolbar.vue";
 import LayoutTagsView from "./components/LayoutTagsView.vue";
 import LayoutMain from "./components/LayoutMain.vue";
 
-const { showTagsView, showLogo, routes } = useLayout();
+const { showTagsView, showLogo } = useLayout();
 const { width } = useWindowSize();
+
+const permissionStore = usePermissionStore();
+
+const topMenuItems = computed(() => {
+  return permissionStore.routes.filter((item) => !item.meta?.hidden);
+});
 
 const isLogoCollapsed = computed(() => width.value < 768);
 </script>
@@ -56,8 +67,44 @@ const isLogoCollapsed = computed(() => width.value < 768);
     }
 
     &-left {
+      display: flex;
       flex: 1;
+      align-items: center;
+      min-width: 0;
       padding-left: 20px;
+    }
+
+    &-logo {
+      display: flex;
+      flex-shrink: 0;
+      align-items: center;
+      height: 100%;
+
+      :deep(.logo) {
+        height: $navbar-height;
+      }
+    }
+
+    &-menu {
+      display: flex;
+      flex: 1;
+      align-items: center;
+      min-width: 0;
+      height: 100%;
+      overflow: hidden;
+
+      :deep(.el-menu) {
+        height: 100%;
+        background-color: transparent;
+        border: none;
+      }
+
+      :deep(.el-menu--horizontal) {
+        flex: 1;
+        min-width: 0;
+        height: $navbar-height;
+        overflow: hidden;
+      }
     }
 
     &-right {
